73ff39fd86aaf7e7018af7779cbcf20823be441f
Saksham <2020UCP1801@mnit.ac.in>
2021-10-04 21:58:44 +0530

    admin page ui enhanced

--- lib/screens/admin_page.dart
+++ lib/screens/admin_page.dart
@@ -45,6 +45,7 @@ class _AdminScreenState extends State<AdminScreen> {
   }
 
   Future<void> _submitForm() async {
+    SystemChannels.textInput.invokeMethod('TextInput.hide');
     setState(() {
       _isLoading = true;
     });
@@ -56,10 +57,45 @@ class _AdminScreenState extends State<AdminScreen> {
         'price': _price,
         'description': _desc,
       });
+      print({
+        'img': _img,
+        'name': _name,
+        'price': _price,
+        'description': _desc,
+      });
     }
     setState(() {
       _isLoading = false;
     });
+  }
+
+  Container getFormInput(
+    String hintText,
+    TextInputType inputType,
+    Function validator,
+    Function onSaved,
+  ) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
+      margin: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 5),
+      child: TextFormField(
+        decoration: InputDecoration.collapsed(
+          hintText: hintText,
+          hintStyle: TextStyle(
+            color: Colors.blue[900],
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        keyboardType: inputType,
+        style: TextStyle(color: Colors.pink[900]),
+        validator: (val) => validator(val),
+        onSaved: (val) => onSaved(val),
+      ),
+    );
   }
 
   @override
@@ -68,72 +104,106 @@ class _AdminScreenState extends State<AdminScreen> {
       appBar: AppBar(
         title: Text("Admin"),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(25),
-          ),
-          margin: EdgeInsets.all(20),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.all(16),
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      Text(
-                        "Add a dish",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      TextFormField(
-                        key: ValueKey("img"),
-                        decoration: InputDecoration(labelText: "Image URL"),
-                        validator: (val) => _validateImg(val!),
-                        onSaved: (val) => _img = val!,
-                      ),
-                      TextFormField(
-                        key: ValueKey("name"),
-                        decoration: InputDecoration(labelText: "Name"),
-                        textCapitalization: TextCapitalization.words,
-                        validator: (val) => _validateName(val!),
-                        onSaved: (val) => _name = val!,
-                      ),
-                      TextFormField(
-                        key: ValueKey("price"),
-                        decoration:
-                            InputDecoration(labelText: "Price (in Rs.)"),
-                        keyboardType: TextInputType.number,
-                        validator: (val) => _validatePrice(val!),
-                        onSaved: (val) => _price = val!,
-                      ),
-                      TextFormField(
-                        key: ValueKey("desc"),
-                        decoration: InputDecoration(labelText: "Description"),
-                        textCapitalization: TextCapitalization.sentences,
-                        keyboardType: TextInputType.multiline,
-                        maxLines: 2,
-                        validator: (val) => _validateDesc(val!),
-                        onSaved: (val) => _desc = val!,
-                      ),
-                      SizedBox(height: 15),
-                      _isLoading
-                          ? Center(child: CircularProgressIndicator())
-                          : RaisedButton(
-                              child: Text("Add"),
-                              onPressed: _submitForm,
-                            ),
-                    ],
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                getFormInput(
+                  "Image URL",
+                  TextInputType.url,
+                  _validateImg,
+                  (val) => {_img = val},
+                ),
+                getFormInput(
+                  "Name of Dish",
+                  TextInputType.name,
+                  _validateName,
+                  (val) => {_name = val},
+                ),
+                getFormInput(
+                  "Price (in Rs)",
+                  TextInputType.number,
+                  _validatePrice,
+                  (val) => {_price = val},
+                ),
+                getFormInput(
+                  "Description",
+                  TextInputType.text,
+                  _validateDesc,
+                  (val) => {_desc = val},
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ButtonTheme(
+                  // minWidth: deviceSize.width * 0.85,
+                  buttonColor: Colors.blue[900],
+                  padding: EdgeInsets.all(10),
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    textColor: Colors.white,
+                    onPressed: _submitForm,
+                    child: Text(
+                      'Submit',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
+            // child: Column(
+            //   children: [
+            //     Text(
+            //       "Add a dish",
+            //       style: TextStyle(
+            //         fontSize: 20,
+            //         fontWeight: FontWeight.bold,
+            //       ),
+            //     ),
+            //     SizedBox(height: 10),
+            //     TextFormField(
+            //       key: ValueKey("img"),
+            //       decoration: InputDecoration(labelText: "Image URL"),
+            //       validator: (val) => _validateImg(val!),
+            //       onSaved: (val) => _img = val!,
+            //     ),
+            //     TextFormField(
+            //       key: ValueKey("name"),
+            //       decoration: InputDecoration(labelText: "Name"),
+            //       textCapitalization: TextCapitalization.words,
+            //       validator: (val) => _validateName(val!),
+            //       onSaved: (val) => _name = val!,
+            //     ),
+            //     TextFormField(
+            //       key: ValueKey("price"),
+            //       decoration:
+            //           InputDecoration(labelText: "Price (in Rs.)"),
+            //       keyboardType: TextInputType.number,
+            //       validator: (val) => _validatePrice(val!),
+            //       onSaved: (val) => _price = val!,
+            //     ),
+            //     TextFormField(
+            //       key: ValueKey("desc"),
+            //       decoration: InputDecoration(labelText: "Description"),
+            //       textCapitalization: TextCapitalization.sentences,
+            //       keyboardType: TextInputType.multiline,
+            //       maxLines: 2,
+            //       validator: (val) => _validateDesc(val!),
+            //       onSaved: (val) => _desc = val!,
+            //     ),
+            //     SizedBox(height: 15),
+            //     _isLoading
+            //         ? Center(child: CircularProgressIndicator())
+            //         : RaisedButton(
+            //             child: Text("Add"),
+            //             onPressed: _submitForm,
+            //           ),
+            //   ],
+            // ),
           ),
         ),
       ),
